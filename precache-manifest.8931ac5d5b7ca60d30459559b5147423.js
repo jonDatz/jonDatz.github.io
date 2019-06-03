de10c02af97d051698ab4b5d604f3fa6eb2d36ff
jonDatz <notjondatz@gmail.com>
2019-06-02 20:17:37 -0400

    Updates

---
+++ precache-manifest.8931ac5d5b7ca60d30459559b5147423.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2be29497d4d55c8ad983e6ffa755f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ff598e2ddd023d8c1cd",
+    "url": "/static/css/main.04326a04.chunk.css"
+  },
+  {
+    "revision": "1ac3c2995384d8e21696",
+    "url": "/static/js/2.2a9513b4.chunk.js"
+  },
+  {
+    "revision": "4ff598e2ddd023d8c1cd",
+    "url": "/static/js/main.befd74e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b5eeacdef0a604b9aa567175abd27464",
+    "url": "/static/media/crystalCollector.b5eeacde.png"
+  },
+  {
+    "revision": "7db2a983eeec4a14a2acf8e058e81ffe",
+    "url": "/static/media/gif-collector.7db2a983.png"
+  },
+  {
+    "revision": "05820cb9671e6f7226691babb7905b27",
+    "url": "/static/media/jawnit.05820cb9.png"
+  },
+  {
+    "revision": "36ec3d7f22acd4298774f28be51450f0",
+    "url": "/static/media/me.36ec3d7f.jpg"
+  },
+  {
+    "revision": "7913c10a8ec598097a690597c8a53be5",
+    "url": "/static/media/pokemon-trivia.7913c10a.png"
+  },
+  {
+    "revision": "e49f7842242a35cf3818fba3cf7af87c",
+    "url": "/static/media/project1.e49f7842.jpg"
+  },
+  {
+    "revision": "2538a1e3e612fde5b5743dc518805922",
+    "url": "/static/media/reactReading.2538a1e3.png"
+  },
+  {
+    "revision": "5c7a69d74a694265542ae6bdc59318db",
+    "url": "/static/media/traee.5c7a69d7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0fd71e28f746a4ecc6f110789d8e2b63.js"
+  "/precache-manifest.8931ac5d5b7ca60d30459559b5147423.js"
 );
 
 self.addEventListener('message', (event) => {
